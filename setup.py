920bbbc5d7fec6df8b826eb062b7aa590a1184b8
Tobias <pito.online@gmail.com>
2021-11-19 16:58:36 +0100

    Add setup,py slight renaming

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(
+    name='Pyspatialkit',
+    version='0.0.1',
+    author='Tobias Pietz',
+    author_email='pito.mailing@gmail.com',
+    packages=['pyspatialkit'],
+    license='TODO',#TODO
+    description='TDODO',
+    long_description="TODO",#TODO
+    install_requires=[],#TODO
+)
