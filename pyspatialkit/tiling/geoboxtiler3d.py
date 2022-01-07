05f8b0e49077820a2b86385402d275b197cb33a1
Tobias <pito.online@gmail.com>
2022-01-07 02:04:23 +0100

    Add GeoBoxTiler3d

---
+++ pyspatialkit/tiling/geoboxtiler3d.py
@@ -0,0 +1,78 @@
+from typing import Tuple, List, Union, Optional
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+from ..dataobjects.geoshape import GeoShape
+from ..spacedescriptors.geobox3d import GeoBox3d
+from ..spacedescriptors.collections.geobox3dcollection import GeoBox3dCollection
+from ..dataobjects.geoshape import GeoShape
+from ..crs.geocrs import GeoCrs
+from ..utils.tiling import bounds_from_polygon, raster_bounds2d, raster_bounds3d
+
+class GeoBoxTile3dIterator:
+
+    def __init__(self, geobox_tiler_3d: 'GeoBoxTiler3d'):
+        self.tiler = geobox_tiler_3d
+        self.current = 0
+
+    @abstractmethod
+    def __iter__(self) -> 'GeoBoxTile3dIterator':
+        self
+
+    @abstractmethod
+    def __next__(self) -> GeoBox3d:
+        if self.current >= len(self.tiler.tiles):
+            raise StopIteration
+        tmp = self.tiler.tiles[self.current]
+        self.current+=1
+        return tmp
+
+
+class GeoBoxTiler3d:
+
+    def __init__(self, aoi: GeoShape, min_height: float, max_height: float,
+                 raster_size: Union[Tuple[float, float], Tuple[float, float, float]],
+                 reference_crs: Optional[GeoCrs] = None, border_size:Union[Tuple[float, float], Tuple[float, float, float]]=(0,0,0)):
+        self._tiles = None
+        self.aoi = aoi
+        self.raster_size = np.array(raster_size)
+        self.height_span = np.array((min_height, max_height))
+        if reference_crs is None:
+            self.crs = self.aoi.crs
+        else:
+            self.crs = reference_crs
+        if self.raster_size.shape == 3:
+            if len(border_size) == 2:
+                self.border_size = np.array([border_size[0], border_size[1], 0])
+            else:
+                self.border_size = np.array(border_size)
+        else:
+            self.border_size = np.array(border_size)[:2]
+
+    @abstractmethod
+    def _generate_tiles(self) -> GeoBox3dCollection:
+        bounds = bounds_from_polygon(self.aoi)
+        if self.raster_size.shape[0] == 3:
+            for i in range(len(bounds)):
+                b = bounds[i]
+                bounds[i] = (b[0], b[1], self.height_span[0], b[2], b[3], self.height_span[1])
+        tiles = []
+        inner_size = self.raster_size - 2 * self.border_size
+        if self.raster_size.shape[0] == 2:
+            origins = raster_bounds2d(bounds, self.raster_size, border_size=self.border_size)
+            mins = np.concatenate((origins, np.full((origins.shape[0], 1), self.height_span[0])), axis=1)
+            maxs = mins + np.array([self.raster_size[0], self.raster_size[1], self.height_span[1] - self.height_span[0]])
+        else:
+            mins = raster_bounds3d(bounds, self.raster_size, border_size=self.border_size)
+            maxs = mins + np.array([self.raster_size[0], self.raster_size[1], self.height_span[1] - self.height_span[0]])
+        return GeoBox3dCollection.from_mins_maxs(mins, maxs, self.crs)
+
+    def __iter__(self) -> GeoBoxTile3dIterator:
+        return GeoBoxTile3dIterator(self)
+
+    @property
+    def tiles(self):
+        if self._tiles is None:
+            self._tiles = self._generate_tiles()
+        return self._tiles
---
+++ pyspatialkit/utils/tiling.py
@@ -0,0 +1,47 @@
+from typing import List, Tuple
+
+import numpy as np
+from shapely.geometry import MultiLineString, MultiPolygon, MultiPoint
+
+from ..dataobjects.geoshape import GeoShape
+
+def bounds_from_polygon(polygon: GeoShape):
+    bounds = []
+    if isinstance(polygon, (MultiLineString, MultiPolygon, MultiPoint)):
+        for geom in polygon:
+            bounds.append(geom.bounds)
+    else:
+        bounds = [polygon.bounds]
+    return bounds
+
+def raster_bounds2d(bounds: List[Tuple[float, float, float, float]], raster_size: Tuple[float, float],
+                    border_size: Tuple[float, float] = (0,0)) -> np.ndarray:
+    """
+        returns: origins of rasters as np.ndarray of shape (n,2)
+    """
+    raster_size = np.array(raster_size)
+    border_size = np.array(border_size)
+    inner_size = raster_size - 2 * border_size
+    origins = []
+    for min_x, min_y, max_x, max_y in bounds:
+            xs = np.arange(min_x, max_x, inner_size[0])
+            ys = np.arange(min_y, max_y, inner_size[1])
+            origins.append(np.stack(np.meshgrid(xs, ys, indexing='ij'),axis=2).reshape((-1,2)))
+    return np.concatenate(origins, axis=0)
+
+
+def raster_bounds3d(bounds: List[Tuple[float, float, float, float, float, float]], raster_size: Tuple[float, float, float],
+                    border_size: Tuple[float, float, float] = (0,0, 0)) -> np.ndarray:
+    """
+        returns: origins of rasters as np.ndarray of shape (n,3)
+    """
+    raster_size = np.array(raster_size)
+    border_size = np.array(border_size)
+    inner_size = raster_size - 2 * border_size
+    origins = []
+    for min_x, min_y, min_z, max_x, max_y, max_z in bounds:
+            xs = np.arange(min_x, max_x, inner_size[0])
+            ys = np.arange(min_y, max_y, inner_size[1])
+            zs = np.arange(min_z, max_z, inner_size[1])
+            origins.append(np.stack(np.meshgrid(xs, ys, zs, indexing='ij'),axis=3).reshape((-1,3)))
+    return np.concatenate(origins, axis=0)
