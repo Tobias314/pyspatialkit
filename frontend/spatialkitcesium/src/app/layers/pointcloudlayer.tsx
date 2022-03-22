5335d4f49f94cca0c4ef6766baa2ba30cb7f39ac
Tobias <pito.online@gmail.com>
2022-03-22 20:56:39 +0100

    Point cloud styling

--- frontend/spatialkitcesium/src/app/layers/pointcloudlayer.tsx
+++ frontend/spatialkitcesium/src/app/layers/pointcloudlayer.tsx
@@ -1,20 +1,26 @@
 import React from "react";
 import { Cesium3DTileset } from "resium";
+import { Cesium3DTileStyle, PointCloudShading} from "cesium"
 import { LayerInterface } from "./layerinterface";
 import { BACKEND_URL } from "../constants";
 
 
 export class PointCloudLayer implements LayerInterface{
+    name: string;
+    tilesetUrl: string;
+    style: Cesium3DTileStyle;
+    shading: PointCloudShading
 
     constructor(name: string){
         this.name = name;
         this.tilesetUrl = `${BACKEND_URL}/${this.name}/tiles/root.json`
+        this.style = new Cesium3DTileStyle({
+                pointSize: 10
+            });
+        this.shading = new PointCloudShading({eyeDomeLighting:true})
     }
 
-    name: string;
-    tilesetUrl: string;
-
     toResiumComponent(): any{
-        return (<Cesium3DTileset url={this.tilesetUrl}/>)
+        return (<Cesium3DTileset url={this.tilesetUrl} style={this.style} pointCloudShading={this.shading}/>)
     }
 }
